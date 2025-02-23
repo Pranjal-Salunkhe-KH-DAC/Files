c500577b07bc7e7db687c043671c8f0d12631649
Pranjal-Salunkhe-KH-DAC <pranjalsalunkhe2@gmail.com>
2025-02-23 22:52:05 +0530

    Add files via upload

---
+++ Logic Building Assignments/Assignment 3_Section 2(Guess the Output)/Codes_Section2/DoWhileLoop.java
@@ -0,0 +1,13 @@
+//Snippet 4:  
+
+public class DoWhileLoop { 
+    public static void main(String[] args) { 
+        int i = 1; 
+        do { 
+            System.out.print(i + " "); 
+            i++; 
+        } while (i < 5); 
+        System.out.println(i); 
+    } 
+} 
+ 
