201ecbd6652ebf8f8a119efe611ff30e54efde55
Pranjal-Salunkhe-KH-DAC <pranjalsalunkhe2@gmail.com>
2025-02-23 22:38:41 +0530

    Delete Java Assignment 1 directory

--- Java Assignment 1/Assignment 1/Codes/GMmsg.java
+++
@@ -1,9 +0,0 @@
-//Q4. Display Good Morning Message Based on Time
-class GMmsg{
-	public static void main(String args[]){
-		int time=9;
-		if(time>5 && time<=12){
-			System.out.println("Good Morning Boss !!");
-		}	}
-
-}
