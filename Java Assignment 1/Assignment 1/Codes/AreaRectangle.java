dd06013fc19a0b0887af0c029e721d276b653603
Pranjal-Salunkhe-KH-DAC <pranjalsalunkhe2@gmail.com>
2025-02-23 22:36:05 +0530

    Add files via upload

---
+++ Java Assignment 1/Assignment 1/Codes/AreaRectangle.java
@@ -0,0 +1,9 @@
+//Q6. Area of Rectangle
+class AreaRectangle{
+	public static void main(String args[]){
+		int l=9;
+		int b=8;
+		int Area=l*b;
+		System.out.println("Area is : "+Area);
+	}
+}
---
+++ Java Assignment 1/Assignment 1/Codes/GMmsg.java
@@ -0,0 +1,9 @@
+//Q4. Display Good Morning Message Based on Time
+class GMmsg{
+	public static void main(String args[]){
+		int time=9;
+		if(time>5 && time<=12){
+			System.out.println("Good Morning Boss !!");
+		}	}
+
+}
